6afd1c04d6a7f96148fe21979c540c4081d1f767
goomon <jaegoo.jho@gmail.com>
2024-01-21 23:51:30 +0900

    first commit

---
+++ src/test/java/com/github/goomon/concurrency/ConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.goomon.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
